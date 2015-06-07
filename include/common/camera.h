c709cbf13039ef855b1306a8a9eefc4e8461c7ca
byhj <476652422@qq.com>
2015-06-07 10:50:20 +0800

    Add

--- include/common/camera.h
+++ include/common/camera.h
@@ -1,84 +1,133 @@
-#ifndef CAMERA_H
-#define CAMERA_H
+#pragma once
 
+// Std. Includes
+#include <vector>
+
+// GL Includes
+#include <GL/glew.h>
 #include <glm/glm.hpp>
-#include <GL/freeglut.h>
+#include <glm/gtc/matrix_transform.hpp>
 
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// Default camera values
+const GLfloat YAW        = -90.0f;
+const GLfloat PITCH      =  0.0f;
+const GLfloat SPEED      =  3.0f;
+const GLfloat SENSITIVTY =  0.25f;
+const GLfloat ZOOM       =  45.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
 class Camera
 {
 public:
-	Camera()
+	// Camera Attributes
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	// Eular Angles
+	GLfloat Yaw;
+	GLfloat Pitch;
+
+	// Camera options
+	GLfloat MovementSpeed;
+	GLfloat MouseSensitivity;
+	GLfloat Zoom;
+
+	// Constructor with vectors
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), GLfloat yaw = YAW, GLfloat pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
 	{
-		m_pos = glm::vec3(0.0f, 0.0f, 1.0f);
-		m_target = glm::vec3(0.0f, 0.0f, 0.0f);
-		m_up = glm::vec3(0.0f, 1.0f, 0.0f);
-		m_direction = m_target - m_pos;
+		this->Position = position;
+		this->WorldUp = up;
+		this->Yaw = yaw;
+		this->Pitch = pitch;
+		this->updateCameraVectors();
 	}
-	Camera(const glm::vec3 &Pos, const glm::vec3 &Target, const glm::vec3 &Up)
-		:m_pos(Pos), m_target(Target), m_up(Up) 
-	{	
-		m_direction = m_target - m_pos;
+	// Constructor with scalar values
+	Camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY, GLfloat upZ, GLfloat yaw, GLfloat pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
+	{
+		this->Position = glm::vec3(posX, posY, posZ);
+		this->WorldUp = glm::vec3(upX, upY, upZ);
+		this->Yaw = yaw;
+		this->Pitch = pitch;
+		this->updateCameraVectors();
 	}
 
-	~Camera() { };
-
-	Camera& operator = (const Camera &camTemp)
+	// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+	glm::mat4 GetViewMatrix()
 	{
-		m_pos = camTemp.m_pos;
-		m_target = camTemp.m_target;
-		m_up = camTemp.m_up;
-		return (*this);
+		return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
 	}
 
-	glm::vec3 GetCamPos() const 
+	// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+	void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime)
 	{
-		return m_pos;
+		GLfloat velocity = this->MovementSpeed * deltaTime;
+		if (direction == FORWARD)
+			this->Position += this->Front * velocity;
+		if (direction == BACKWARD)
+			this->Position -= this->Front * velocity;
+		if (direction == LEFT)
+			this->Position += this->Right * velocity;
+		if (direction == RIGHT)
+			this->Position -= this->Right * velocity;
 	}
-	glm::vec3 GetCamTarget() const 
+
+	// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+	void ProcessMouseMovement(GLfloat xoffset, GLfloat yoffset, GLboolean constrainPitch = true)
 	{
-		return m_target;
-	}
-	glm::vec3 GetCamUp() const 
-	{
-		return m_up;
-	}
-	
-	void OnKeyboard(int key, GLfloat step)
-	{
-		switch (key)
+		xoffset *= this->MouseSensitivity;
+		yoffset *= this->MouseSensitivity;
+
+		this->Yaw   += xoffset;
+		this->Pitch += yoffset;
+
+		// Make sure that when pitch is out of bounds, screen doesn't get flipped
+		if (constrainPitch)
 		{
-		    case GLUT_KEY_UP:
-			{
-				m_pos += m_direction * step;
-			}
-			break;
-
-			case GLUT_KEY_DOWN:
-			{
-				m_pos -= m_direction  * step;
-			}
-			break;
-
-			case GLUT_KEY_LEFT:
-			{
-				glm::vec3 right  = glm::cross(m_direction, m_up);
-				right = glm::normalize(right);
-				m_pos += right * step;
-			}
-			break;
-
-			case GLUT_KEY_RIGHT:
-			{
-				m_pos -= glm::normalize(glm::cross(m_direction, m_up)) * step;
-			}
-			break;
+			if (this->Pitch > 89.0f)
+				this->Pitch = 89.0f;
+			if (this->Pitch < -89.0f)
+				this->Pitch = -89.0f;
 		}
-	}
-private:
-	glm::vec3 m_pos;
-	glm::vec3 m_target;
-	glm::vec3 m_up;
-	glm::vec3 m_direction;
-};
 
-#endif
+		// Update Front, Right and Up Vectors using the updated Eular angles
+		this->updateCameraVectors();
+	}
+
+	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+	void ProcessMouseScroll(GLfloat yoffset)
+	{
+		if (this->Zoom >= 1.0f && this->Zoom <= 180.0f)
+			this->Zoom -= yoffset;
+		if (this->Zoom <= 1.0f)
+			this->Zoom = 1.0f;
+		if (this->Zoom >= 180.0f)
+			this->Zoom = 180.0f;
+	}
+
+private:
+	// Calculates the front vector from the Camera's (updated) Eular Angles
+	void updateCameraVectors()
+	{
+		// Calculate the new Front vector
+		glm::vec3 front;
+		front.x = cos(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
+		front.y = sin(glm::radians(this->Pitch));
+		front.z = sin(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
+		this->Front = glm::normalize(front);
+		// Also re-calculate the Right and Up vector
+		this->Right = glm::normalize(glm::cross(this->Front, this->WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+		this->Up    = glm::normalize(glm::cross(this->Right, this->Front));
+	}
+};
---
+++ src/ch15-Camera-Control-2/ch15-Camera-Control-2.cpp
@@ -0,0 +1,249 @@
+#include <common/shader.h>
+#include <common/common.h>
+#include <common/camera.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#ifdef WIN32
+//If PLATFORM IS WIN32, we put the render window to the middle of window
+const int g_ScreenWidth = GetSystemMetrics(SM_CXSCREEN) * 0.75;
+const int g_ScreenHeight = GetSystemMetrics(SM_CYSCREEN) * 0.75;
+const GLfloat g_Aspect = float(g_ScreenWidth) / float(g_ScreenHeight);
+const int g_PosX = (GetSystemMetrics(SM_CXSCREEN) - g_ScreenWidth) / 2;
+const int g_PosY = (GetSystemMetrics(SM_CYSCREEN) - g_ScreenHeight) / 2;
+
+#else
+const int g_ScreenWidth = 800;
+const int g_ScreenHeight = 600;
+const int g_PosX = 300;
+const int g_PosY = 100;
+#endif
+
+//Window Title
+const char *g_pWindowTitle = "ch3-Triangle";
+static Shader TriangleShader("Triangle Shader");
+static GLuint g_vbo = 0, g_ibo = 0, g_program = 0, g_vao = 0;
+static GLuint g_mvp_loc = -1;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+static GLfloat g_step = 0.0f;
+
+static const GLfloat VertexData[] = 
+{
+	-1.0f, -1.0f,  0.5773f,
+	 0.0f, -1.0f, -1.15475f,
+	 1.0f, -1.0f,  0.5773f,
+	 0.0f,  1.0f,  0.0f,
+};
+static const GLsizei VertexSize = sizeof(VertexData);
+
+static const GLuint IndexData[] = 
+{
+	0, 3, 1,
+	1, 3, 2,
+	2, 3, 0,
+	0, 1, 2
+};
+static const GLsizei IndexSize = sizeof(IndexData);
+
+void init_buffer()
+{
+	//vbo are buffers that can be stored in video memory and provide the shortest access time to the GPU
+	glGenBuffers(1, &g_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
+	glBufferData(GL_ARRAY_BUFFER, VertexSize, VertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glGenBuffers(1, &g_ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, IndexSize, IndexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void init_vertexArray()
+{
+	//vao is manage the opengl status
+	glGenVertexArrays(1, &g_vao);
+	glBindVertexArray(g_vao);
+
+	//We bind the buffer, change vao status
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
+	glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
+
+	//Hint how opengl send the data
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);	
+	
+	//disable it before use
+	glBindVertexArray(0);
+	glDisableVertexAttribArray(0);
+	
+}
+
+void init_shader()
+{
+	TriangleShader.init();
+	TriangleShader.attach(GL_VERTEX_SHADER, "triangle.vert");
+	TriangleShader.attach(GL_FRAGMENT_SHADER, "triangle.frag");
+	TriangleShader.link();
+	g_program = TriangleShader.GetProgram();
+	g_mvp_loc = glGetUniformLocation(g_program, "mvp");
+	if (g_mvp_loc == -1)
+	{
+		std::cerr << "Can not get the uniform location." << std::endl;
+	}
+
+}
+
+void init()
+{
+
+#ifdef _DEBUG
+	videoCardInfo();
+#endif
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		std::cerr << "Error:" << glewGetErrorString(res) << std::endl;
+		return;
+	}
+	glEnable(GL_CULL_FACE);
+	init_buffer();
+	init_vertexArray();
+	init_shader();
+
+	//set the background color 
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+
+void render()
+{
+	//clear the color buffer to backgroud color
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//We use current shader program and vao status to render the scene
+	glUseProgram(g_program);
+	glBindVertexArray(g_vao);
+
+	static GLfloat time = 0.0f;
+	time += 0.1f;
+	static glm::mat4 mone = glm::mat4(1.0f);
+
+	//You should add the translate to set last
+	glm::mat4 world = glm::translate(mone, glm::vec3(0.0f, 0.0f, -5.0f) )
+		* glm::rotate(mone, glm::radians(time), glm::vec3(0.0f, 1.0f, 0.0f) );
+	static float currentTime = 0.0f;
+	currentTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+	deltaTime = currentTime - lastFrame;
+	deltaTime *= 10.0f;
+	lastFrame = currentTime;
+
+	//glm::mat4 view = glm::lookAt(g_Camera.GetCamPos(), g_Camera.GetCamTarget(), g_Camera.GetCamUp());
+	//glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	glm::mat4 view = camera.GetViewMatrix();
+	glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), g_Aspect, 1.0f, 100.0f);
+	glm::mat4 mvp = proj * view * world;
+
+					//Notice the row-major or column-major 
+	glUniformMatrix4fv(0, 1, GL_FALSE, &mvp[0][0] );
+	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+	//Swap the buffer to show and make current window rediaplay
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void keyboard(int key, int x, int y)
+{
+	switch (key)
+	{
+	    case 
+	    case GLUT_KEY_UP:
+			camera.ProcessKeyboard(FORWARD, deltaTime);
+		break;
+
+		case GLUT_KEY_DOWN:
+			camera.ProcessKeyboard(BACKWARD, deltaTime);
+		break;
+
+		case GLUT_KEY_LEFT:
+			camera.ProcessKeyboard(LEFT, deltaTime);
+	    break;
+
+		case GLUT_KEY_RIGHT:
+			camera.ProcessKeyboard(RIGHT, deltaTime);
+		break;
+	}
+	
+}
+
+int lastDir = 1;
+void mouseWheel(int wheel, int direction, int x, int y)
+{
+
+	if (lastDir != direction)
+		g_step = 0.0;
+	g_step += direction;
+	std::cout << g_step << std::endl;
+	camera.ProcessMouseScroll(g_step);
+	lastDir = direction;
+}
+
+void passiveMouse(int xpos, int ypos)
+{
+		if(firstMouse)
+		{
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+
+		GLfloat xoffset = xpos - lastX;
+		GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+
+		lastX = xpos;
+		lastY = ypos;
+
+		camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void shutdown()
+{ 
+	glDeleteProgram(g_program);
+	glDeleteVertexArrays(1, &g_vao);
+	glDeleteBuffers(1, &g_vbo);
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(g_ScreenWidth, g_ScreenHeight);
+	glutInitWindowPosition(g_PosX, g_PosY);
+	glutCreateWindow(g_pWindowTitle);
+	init();
+	glutDisplayFunc(render);
+	glutSpecialFunc(keyboard);
+	glutPassiveMotionFunc(passiveMouse);
+	glutMouseWheelFunc(mouseWheel);
+	glutIdleFunc(render);
+	glutMainLoop();
+	shutdown();
+}
